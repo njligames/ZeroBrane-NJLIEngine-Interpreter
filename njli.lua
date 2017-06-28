65afd97f879f0b298d4e6b95695b351a600aa70e
njligames <jamesfolk1@gmail.com>
2017-06-28 10:48:20 -0400

    initial attempt to write the interpreter

--- njli.lua
+++ njli.lua
@@ -1,20 +1,20 @@
 -- Copyright 2011-12 Paul Kulchenko, ZeroBrane LLC
 
-local macOS
+local njli
 local win = ide.osname == "Windows"
 local mac = ide.osname == "Macintosh"
 
 return {
-  name = "NJLI",
+  name = "NJLIGameEngine",
   description = "NJLI game engine",
-  api = {"baselib", "macOS"},
+  api = {"baselib", "njli"},
   frun = function(self,wfilename,rundebug)
-    macOS = macOS or ide.config.path.macOS -- check if the path is configured
-    if not macOS then
+    njli = njli or ide.config.path.njli -- check if the path is configured
+    if not njli then
       local sep = win and ';' or ':'
       local default =
-           win and (GenerateProgramFilesPath('macOS', sep)..sep)
-        or mac and ('/Applications/macOS.app/Contents/MacOS'..sep)
+           win and (GenerateProgramFilesPath('NJLIGameEngine', sep)..sep)
+        or mac and ('/Applications/NJLIGameEngine.app/Contents/MacOS'..sep)
         or ''
       local path = default
                  ..(os.getenv('PATH') or '')..sep
@@ -22,10 +22,10 @@ return {
                  ..(os.getenv('HOME') and GetPathWithSep(os.getenv('HOME'))..'bin' or '')
       local paths = {}
       for p in path:gmatch("[^"..sep.."]+") do
-        macOS = macOS or GetFullPathIfExists(p, win and 'macOS.exe' or 'macOS')
+        njli = njli or GetFullPathIfExists(p, win and 'NJLIGameEngine.exe' or 'NJLIGameEngine')
         table.insert(paths, p)
       end
-      if not macOS then
+      if not njli then
         DisplayOutputLn("Can't find njli executable in any of the following folders: "
           ..table.concat(paths, ", "))
         return
@@ -35,14 +35,30 @@ return {
       end
     end
 
-    if not GetFullPathIfExists(self:fworkdir(wfilename), 'main.lua') then
-      DisplayOutputLn(("Can't find 'main.lua' file in the current project folder: '%s'.")
-        :format(self:fworkdir(wfilename)))
-      return
+--    if not GetFullPathIfExists(self:fworkdir(wfilename), 'main.lua') then
+--      DisplayOutputLn(("Can't find 'main.lua' file in the current project folder: '%s'.")
+--        :format(self:fworkdir(wfilename)))
+--      return
+--    end
+
+    local file
+    local epoints = ide.config.njli and ide.config.njli.entrypoints
+    if epoints then
+      epoints = type(epoints) == 'table' and epoints or {epoints}
+      for _,entry in pairs(epoints) do
+        file = GetFullPathIfExists(self:fworkdir(wfilename), entry)
+        if file then break end
+      end
+      if not file then
+        DisplayOutputLn("Can't find any of the specified entry points ("
+          ..table.concat(epoints, ", ")
+          ..") in the current project; continuing with the current file...")
+      end
     end
 
     if rundebug then
-      DebuggerAttachDefault({runstart = ide.config.debugger.runonstart == true})
+      DebuggerAttachDefault({rstartwith = file,
+        unstart = ide.config.debugger.runonstart == true})
     end
 
     -- suppress hiding ConsoleWindowClass as this is used by Love console
@@ -50,8 +66,8 @@ return {
     local cwc = uhw and uhw.ConsoleWindowClass
     if uhw then uhw.ConsoleWindowClass = 0 end
 
-    local params = ide.config.arg.any or ide.config.arg.macOS
-    local cmd = ('"%s" "%s"%s%s'):format(macOS, self:fworkdir(wfilename),
+    local params = ide.config.arg.any or ide.config.arg.njli
+    local cmd = ('"%s" "%s"%s%s'):format(njli, self:fworkdir(wfilename),
       params and " "..params or "", rundebug and ' -debug' or '')
     DisplayOutputLn('This is the command: ' .. cmd )
     -- CommandLineRun(cmd,wdir,tooutput,nohide,stringcallback,uid,endcallback)
